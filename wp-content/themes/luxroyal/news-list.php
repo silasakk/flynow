9730955daacda3901be32a83c12634d4ad9b230a
Silasak Lawong <silasak.lf@gmail.com>
2015-02-17 16:38:06 +0700

    sync

---
+++ wp-content/themes/luxroyal/news-list.php
@@ -0,0 +1,121 @@
+<?php include('header.inc.php') ?>
+<div class="news-banner"></div>
+<div class="content">
+
+    <div class="content-nav">
+        <div class="container">
+            <div class="content-nav-title text-title">NEWS & EVENTS</div>
+            <div class="content-nav-menu pull-right">
+                <ul>
+                    <li>HOME</li>
+                    <li>NEWS & EVENTS</li>
+                    
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <ul class="tag-list">
+            <li><a href="#">ALL</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">EVENTS</a></li>
+        </ul>
+        <div class="clearfix"></div>
+        <ul class="news-list">
+            <li class="col-8 col-sm-12">
+                <div class="con-warp">
+                    <img src="images/demo-list-news1.png" />
+                    <div class="caption">
+                        <h2>LUX AWARDS 2014</h2>
+                        <p class="dtl">
+                            Every line of code crafted to perfection, optimized for super fast performance 
+                            and is cross browser compatible.
+                        </p>
+                        <p>News , 15  Aug  2015 <span class="pull-right">- more -</span></p>
+                    </div>
+
+                </div>
+            </li>
+            <li class="col-4 col-sm-12 xxx">
+                <div class="con-warp">
+                    <img src="images/demo-list-news2.png" />
+                    <div class="caption">
+                        <h2>LUX AWARDS 2014</h2>
+                        <p class="dtl">
+                            Every line of code crafted to perfection, optimized for super fast performance 
+                            and is cross browser compatible.
+                        </p>
+                        <p>News , 15  Aug  2015 <span class="pull-right">- more -</span></p>
+                    </div>
+                </div>
+            </li>
+            <li class="col-4 col-sm-12 xxx">
+                <div class="con-warp">
+                    <img src="images/demo-list-news2.png" />
+                    <div class="caption">
+                        <h2>LUX AWARDS 2014</h2>
+                        <p class="dtl">
+                            Every line of code crafted to perfection, optimized for super fast performance 
+                            and is cross browser compatible.
+                        </p>
+                        <p>News , 15  Aug  2015 <span class="pull-right">- more -</span></p>
+                    </div>
+                </div>
+            </li>
+            <li class="col-8 col-sm-12">
+                <div class="con-warp">
+                    <img src="images/demo-list-news1.png" />
+                    <div class="caption">
+                        <h2>LUX AWARDS 2014</h2>
+                        <p class="dtl">
+                            Every line of code crafted to perfection, optimized for super fast performance 
+                            and is cross browser compatible.
+                        </p>
+                        <p>News , 15  Aug  2015 <span class="pull-right">- more -</span></p>
+                    </div>
+
+                </div>
+            </li>
+            <li class="col-8 col-sm-12">
+                <div class="con-warp">
+                    <img src="images/demo-list-news1.png" />
+                    <div class="caption">
+                        <h2>LUX AWARDS 2014</h2>
+                        <p class="dtl">
+                            Every line of code crafted to perfection, optimized for super fast performance 
+                            and is cross browser compatible.
+                        </p>
+                        <p>News , 15  Aug  2015 <span class="pull-right">- more -</span></p>
+                    </div>
+
+                </div>
+            </li>
+            <li class="col-4 col-sm-12 xxx">
+                <div class="con-warp">
+                    <img src="images/demo-list-news2.png" />
+                    <div class="caption">
+                        <h2>LUX AWARDS 2014</h2>
+                        <p class="dtl">
+                            Every line of code crafted to perfection, optimized for super fast performance 
+                            and is cross browser compatible.
+                        </p>
+                        <p>News , 15  Aug  2015 <span class="pull-right">- more -</span></p>
+                    </div>
+                </div>
+            </li>
+            
+        </ul>
+        <div class="clearfix"></div>
+        <ul class="tag-list">
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">...</a></li>
+            <li><a href="#">25</a></li>
+        </ul>
+        <div class="clearfix"></div>
+    </div>
+
+
+</div>
+<?php include('footer.inc.php') ?>
