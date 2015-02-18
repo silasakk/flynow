aeba98ba1c86f84ba2ba3860d8c089e341a791e2
Silasak Lawong <silasak.lf@gmail.com>
2015-02-18 11:45:36 +0700

    sync

---
+++ wp-content/themes/luxroyal/blog.php
@@ -0,0 +1,134 @@
+<?php include('header.inc.php') ?>
+<div class="content">
+
+    <div class="content-nav">
+        <div class="container">
+            <div class="content-nav-title text-title">BLOG</div>
+            <div class="content-nav-menu pull-right">
+                <ul>
+                    <li>HOME</li>
+                    <li>BLOG</li>
+                    <li>SINGLE IMAGE</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="blog-top">
+            <div class="col-9">
+                <div class="con-warp">
+                    <img src="images/demo-blog-1.png" />
+                    <div class="caption">
+                        <h1>LUX AWARDS 2015</h1>
+                        <p>DATE / 19TH NOVEMBER 2014</p>
+                        <p>
+                            <span><i class="fa fa-bars"></i> ENVIRONMENT</span>
+                            <span>SHARE</span>
+                        </p>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+                <div class="blog-con">
+                    <h1 class="text-title"><strong>LUX AWARDS 2015</strong></h1>
+                    <p>
+                        How many times do you imagine the average entrepreneur changes or alters his or her business 
+                        plan One big mistake many businesses starting .
+                    </p>
+                    <hr>
+                    <p>
+                        In the beginning of 2015 I developed neEvent which is short for New England event. My plan from the
+                        was to deliver online nightclub, restaurant, lounge, sport, music, family and adult entertainment 
+                        event information and services. I wanted to give the users the ability to get on v.i.p. guest lists for particip
+                        ating events. This was the main focus of my winning strategy which I believed would bring the users and 
+                        <br><br>
+                        eventually make some money. After months of planning, development and testing I launched the site in 
+                        February. I set up newsletters, event alerts and other campaigns to collect email addresses and contact in
+                        formation.
+                        <br><br>
+                        By May things were going better then expected for the short period of time on the web with minimal capi
+                        tal for advertising or marketing and very little search engine exposure.
+                        <br><br>
+                        How else can I make my internet destination sticky, I needed to give the users a reason to come back to 
+                        the site other then to browse events and get on guest lists.
+                        <br><br>
+                        <img src="images/news-demo1.png" />
+                    </p>
+                </div>
+                
+                <p class="blog-relate-title text-title center"><strong>Might Be Interesting</strong></p>
+                
+                <div class="blog-relate2">
+                    <ul>
+                        <li class="col-6">
+                            <div class="blog-relate2-item">
+                                <img src="images/demo-relate1.png">
+                                <div class="caption">
+                                    <div class="pull-left" style="width:60%"><strong>LUX LUNCH NEW WEBSITE GRAND</strong></div>
+                                    <div class="pull-right center" style="width:40%">19 NOV 2014</div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="col-6">
+                            <div class="blog-relate2-item">
+                                <img src="images/demo-relate2.png.png">
+                                <div class="caption">
+                                    <div class="pull-left" style="width:60%"><strong>LUX LUNCH NEW WEBSITE GRAND</strong></div>
+                                    <div class="pull-right center" style="width:40%">19 NOV 2014</div>
+                                </div>
+                                <div class="caption caption-bar">
+                                    <span class="sc">
+                                        <i class="fa fa-twitter"></i>
+                                        <i class="fa fa-facebook"></i>
+                                        <i class="fa fa-google-plus"></i>
+                                    </span>
+                                    
+                                    <span>
+                                        <i class="fa fa-bars"></i> ENVIRONMENT
+                                    </span>
+                                    <span class="readmore">
+                                        <a href="#">READ MORE</a>
+                                    </span>
+                                    
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="blog-recent">
+                    <h1><strong>RECENT</strong> BLOG</h1>
+                    <ul>
+                        <li>
+                            <img src="images/demo-blog-2.png" />
+                            <p class="title">LUX AWARDS 2015<p>
+                            <p class="date">28 - DECEMBER - 2014</p>
+                            <p><i class="fa fa-bars"></i> ENVIRONMENT</p>
+                        </li>
+                        <li>
+                            <img src="images/demo-blog-2.png" />
+                            <p class="title">LUX AWARDS 2015<p>
+                            <p class="date">28 - DECEMBER - 2014</p>
+                            <p><i class="fa fa-bars"></i> ENVIRONMENT</p>
+                        </li>
+                        <li>
+                            <img src="images/demo-blog-2.png" />
+                            <p class="title">LUX AWARDS 2015<p>
+                            <p class="date">28 - DECEMBER - 2014</p>
+                            <p><i class="fa fa-bars"></i> ENVIRONMENT</p>
+                        </li>
+                        <li>
+                            <img src="images/demo-blog-2.png" />
+                            <p class="title">LUX AWARDS 2015<p>
+                            <p class="date">28 - DECEMBER - 2014</p>
+                            <p><i class="fa fa-bars"></i> ENVIRONMENT</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+        
+    </div>
+</div>
+<?php include('footer.inc.php') ?>
