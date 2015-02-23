c54b8f1253d54009195251c7f185b161e7473612
Silasak Lawong <silasak.lf@gmail.com>
2015-02-23 13:51:00 +0700

    sync

---
+++ wp-content/themes/luxroyal/manual.php
@@ -0,0 +1,73 @@
+<? include('header.inc.php'); ?>
+<div class="manual-banner"></div>
+<div class="cus-menu">
+    <div class="container">
+        <ul>
+            <li class="col-3 "><a href="#">FAQ <i class="fa fa-angle-down"></i></a></li>
+            <li class="col-3 active"><a href="#">MANUAL<i class="fa fa-angle-down"></i></a></li>
+            <li class="col-3"><a href="#">HOW TO<i class="fa fa-angle-down"></i></a></li>
+            <li class="col-3"><a href="#">REQUEST FORM <i class="fa fa-angle-down"></i></a></li>
+        </ul>
+    </div>
+</div>
+<div class="content-manual content ">
+    <div class="content-nav">
+        <div class="container">
+            <div class="content-nav-title text-title">MANUAL</div>
+            <div class="content-nav-menu pull-right">
+                <ul>
+                    <li>HOME</li>
+                    <li>CUSTOMERSERVICE</li>
+                    <li>MANUAL</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <ul class="manual-list">
+            <li class="col-6">
+                <div class="well-content">
+                    <div class="well-left">
+                        <p class="text-title"><strong>LUX POE MANUAL</strong></p>
+                        <p class="text-detail">
+                            The European languages are members of the 
+                            same family. Their separate existence is a myth. 
+                            For science, music, sport, etc, Europe uses the
+                            same vocabulary...
+                        </p>
+                    </div>
+                    <div class="well-right">
+                        <div>
+                            <img src="images/Literature.png" /> 
+                            <p><span class="info">PDF</span> FORMAT</p>
+                            <p><span class="filesize">FILE SIZE 10 MB.</span></p>
+                            <a href="#" class="btn-download">DOWNLOAD</a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="col-6">
+               <div class="well-content">
+                   <div class="well-left">
+                       <p class="text-title"><strong>LUX POE MANUAL</strong></p>
+                       <p class="text-detail">
+                           The European languages are members of the 
+                           same family. Their separate existence is a myth. 
+                           For science, music, sport, etc, Europe uses the
+                           same vocabulary...
+                       </p>
+                   </div>
+                   <div class="well-right">
+                       <div>
+                           <img src="images/Literature.png" /> 
+                           <p><span class="info">PDF</span> FORMAT</p>
+                           <p><span class="filesize">FILE SIZE 10 MB.</span></p>
+                           <a href="#" class="btn-download">DOWNLOAD</a>
+                       </div>
+                   </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
+<? include('footer.inc.php') ?>
