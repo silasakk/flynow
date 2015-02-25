95d7a47da67291f24ea532cc9f967ba6bc551f5a
jeerapa <jeerapa.s@nsec.in.th>
2015-02-25 14:11:45 +0700

    oy

--- wp-content/themes/luxroyal/functions.php
+++ wp-content/themes/luxroyal/functions.php
@@ -269,7 +269,7 @@
 		}
 	}
 
-	/* manage footer */
+	/********************************************************* manage footer*******************************************************/
 	function tutsplus_widgets_init() {
  
 	    // First footer widget area, located in the footer. Empty by default.
@@ -342,4 +342,84 @@
 	 
 	// Register sidebars by running tutsplus_widgets_init() on the widgets_init hook.
 	add_action( 'widgets_init', 'tutsplus_widgets_init' );
+
+
+	/********************************************************* Creating the widget blog*******************************************************/
+	class wpb_widget extends WP_Widget {
+
+		function __construct() {
+			parent::__construct(
+			// Base ID of your widget
+			'wpb_widget', 
+
+			// Widget name will appear in UI
+			__('WPBeginner Widget Blog', 'wpb_widget_domain'), 
+
+			// Widget description
+			array( 'description' => __( 'Sample widget based on WPBeginner Tutorial', 'wpb_widget_domain' ), ) 
+		);
+	}
+
+	// Creating widget front-end
+	// This is where the action happens
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		// before and after widget arguments are defined by themes
+		echo $args['before_widget'];
+		if ( ! empty( $title ) )
+		echo $args['before_title'] . $title . $args['after_title'];
+
+		// This is where you run the code and display the output
+		$args = array(
+				"post_type" 		=> 'blog',
+				"posts_per_page" 	=> '5',
+				"orderby"			=> 'date',
+				"order"				=> 'desc',
+				);
+			// var_dump($args);
+		// The Query
+		$the_query = new WP_Query( $args );
+
+    		if($the_query->have_posts()){
+				$i = 1;
+				echo '<div><ul>';
+				while($the_query->have_posts()){
+					$the_query->the_post();
+					echo '<li><a href="'.get_the_permalink().'">'.get_the_title().'</a></li>';
+				}
+				echo '</ul></div>';
+			}	
+		echo $args['after_widget'];
+	}
+			
+	// Widget Backend 
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+		$title = $instance[ 'title' ];
+		}
+		else {
+		$title = __( 'New title', 'wpb_widget_domain' );
+		}
+		// Widget admin form
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<?php 
+	}
+		
+	// Updating widget replacing old instances with new
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		return $instance;
+		}
+	} // Class wpb_widget ends here
+
+	// Register and load the widget
+	function wpb_load_widget() {
+		register_widget( 'wpb_widget' );
+	}
+	add_action( 'widgets_init', 'wpb_load_widget' );
 ?>
