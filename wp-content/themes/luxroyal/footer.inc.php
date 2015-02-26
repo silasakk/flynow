8ba390d4f336786e1f7be78fd722fd50d2405729
jeerapa <jeerapa.s@nsec.in.th>
2015-02-26 12:00:02 +0700

    oy

--- wp-content/themes/luxroyal/footer.inc.php
+++ wp-content/themes/luxroyal/footer.inc.php
@@ -1,92 +1,181 @@
-	<div id="footer">
+<div id="footer">
     <div class="container">
-        <ul>
-            <li class="col-2">
-                <span><strong>ABOUT US</strong></span>
-                <ul>
-                    <li><a href="#">BRAND OVERVIEW</a></li>
-                    <li><a href="#">HISTORY</a></li>
-                    <li><a href="#">VISION</a></li>
-                    <li><a href="#">MISSION</a></li>
-                    <li><a href="#">CAREER</a></li>
+<?php
+    /* The footer widget area is triggered if any of the areas
+     * have widgets. So let's check that first.
+     *
+     * If none of the sidebars have widgets, then let's bail early.
+     */
+    if (   ! is_active_sidebar( 'first-footer-widget-area'  )
+        && ! is_active_sidebar( 'second-footer-widget-area' )
+        && ! is_active_sidebar( 'third-footer-widget-area'  )
+        && ! is_active_sidebar( 'fourth-footer-widget-area' )
+        && ! is_active_sidebar( 'five-footer-widget-area' )
+        && ! is_active_sidebar( 'six-footer-widget-area' )
+    ): return;
+    else:
+        if (   is_active_sidebar( 'first-footer-widget-area'  )
+            && is_active_sidebar( 'second-footer-widget-area' )
+            && is_active_sidebar( 'third-footer-widget-area'  )
+            && is_active_sidebar( 'fourth-footer-widget-area' )
+            && is_active_sidebar( 'five-footer-widget-area' )
+            && is_active_sidebar( 'six-footer-widget-area' )
+        ) : ?>
+         
+        <ul class="" role="complementary">
+            <li class="col-2 first quarter left widget-area">
+                <?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
+            </li><!-- .first .widget-area -->
+         
+            <li class="col-2 second quarter widget-area">
+                <?php dynamic_sidebar( 'second-footer-widget-area' ); ?>
+            </li><!-- .second .widget-area -->
+         
+            <li class="col-2 third quarter widget-area">
+                <?php dynamic_sidebar( 'third-footer-widget-area' ); ?>
+            </li><!-- .third .widget-area -->
+         
+            <li class="col-2 fourth quarter widget-area">
+                <?php dynamic_sidebar( 'fourth-footer-widget-area' ); ?>
+            </li><!-- .fourth .widget-area -->
 
-                </ul>
-            </li>
-            <li class="col-2">
-                <span><strong>ABOUT US</strong></span>
-                <ul>
-                    <li><a href="#">BRAND OVERVIEW</a></li>
-                    <li><a href="#">HISTORY</a></li>
-                    <li><a href="#">VISION</a></li>
-                    <li><a href="#">MISSION</a></li>
-                    <li><a href="#">CAREER</a></li>
+            <li class="col-2 five quarter widget-area">
+                <?php dynamic_sidebar( 'five-footer-widget-area' ); ?>
+            </li><!-- .fourth .widget-area -->
 
-                </ul>
-            </li>
-            <li class="col-2">
-                <span><strong>ABOUT US</strong></span>
-                <ul>
-                    <li><a href="#">BRAND OVERVIEW</a></li>
-                    <li><a href="#">HISTORY</a></li>
-                    <li><a href="#">VISION</a></li>
-                    <li><a href="#">MISSION</a></li>
-                    <li><a href="#">CAREER</a></li>
-
-                </ul>
-            </li>
-            <li class="col-2">
-                <span><strong>ABOUT US</strong></span>
-                <ul>
-                    <li><a href="#">BRAND OVERVIEW</a></li>
-                    <li><a href="#">HISTORY</a></li>
-                    <li><a href="#">VISION</a></li>
-                    <li><a href="#">MISSION</a></li>
-                    <li><a href="#">CAREER</a></li>
-
-                </ul>
-            </li>
-            <li class="col-2">
-                <span><strong>ABOUT US</strong></span>
-                <ul>
-                    <li><a href="#">BRAND OVERVIEW</a></li>
-                    <li><a href="#">HISTORY</a></li>
-                    <li><a href="#">VISION</a></li>
-                    <li><a href="#">MISSION</a></li>
-                    <li><a href="#">CAREER</a></li>
-
-                </ul>
-            </li>
-            <li class="col-2 col-sm-12">
-                <span><strong>CONTACT US</strong></span>
-                <div class="foot-contact-us">
-                    <span>Lux Royal(Thailand) Co.,Ltd.</span>
-
-                    <span>
-                        523-525 , Sukhumvit 71 Rd., 
-                        Prakanong-Nue, Watana  
-                        10110 Bangkok
-                    </span>
-
-                    <div class="fol">
-                        <span ><img width="12px;" src="images/phone.png" /> Call Center :</span>
-                        <span style="text-align:center">(02) 381-5115</span>
-                        <hr>
-                        <span ><img width="12px;" src="images/email.png" /> EMAIL :</span>
-                        <hr>
-                        <span style="text-align:center">customercare@lux.co.th</span>
-                    </div>
-
+            <li class="col-2 six quarter widget-area">
+                <?php dynamic_sidebar( 'six-footer-widget-area' ); ?>
+                <div class="fol">
+                    <span ><img width="12px;" src="<?php echo get_template_directory_uri() ?>/assets/images/phone.png" /> Call Center :</span>
+                    <span style="text-align:center">(02) 381-5115</span>
+                    <hr>
+                    <span ><img width="12px;" src="<?php echo get_template_directory_uri() ?>/assets/images/email.png" /> EMAIL :</span>
+                    <hr>
+                    <span style="text-align:center">customercare@lux.co.th</span>
                 </div>
-            </li>
-        </ul>
-        <div class="foot2 col-10 col-sm-12">
-            <a class="foot-sitemap" href="#">
-                SITEMAP
-            </a>
-            <br/>
-            <p>Privacy policy and Teams and Condition</p>
-            <img src="images/footlogos.png" />
-        </div>
+            </li><!-- .fourth .widget-area -->
+            <div class="foot2 col-10 col-sm-12">
+                <a class="foot-sitemap" href="#">
+                    SITEMAP
+                </a>
+                <br/>
+                <p>Privacy policy and Teams and Condition</p>
+                <img src="<?php echo get_template_directory_uri() ?>/assets/images/footlogos.png" />
+            </div>
+        </ul><!-- #fatfooter -->
+        <?php 
+        elseif (   is_active_sidebar( 'first-footer-widget-area'  )
+            && is_active_sidebar( 'second-footer-widget-area' )
+            && is_active_sidebar( 'third-footer-widget-area'  )
+            && is_active_sidebar( 'fourth-footer-widget-area' )
+            && is_active_sidebar( 'five-footer-widget-area' )
+            && ! is_active_sidebar( 'six-footer-widget-area' )
+        ) : ?>
+         
+        <ul class="" role="complementary">
+            <li class="col-2 first quarter left widget-area">
+                <?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
+            </li><!-- .first .widget-area -->
+         
+            <li class="col-2 second quarter widget-area">
+                <?php dynamic_sidebar( 'second-footer-widget-area' ); ?>
+            </li><!-- .second .widget-area -->
+         
+            <li class="col-2 third quarter widget-area">
+                <?php dynamic_sidebar( 'third-footer-widget-area' ); ?>
+            </li><!-- .third .widget-area -->
+         
+            <li class="col-2 fourth quarter right widget-area">
+                <?php dynamic_sidebar( 'fourth-footer-widget-area' ); ?>
+            </li><!-- .fourth .widget-area -->
+
+            <li class="col-2 five quarter right widget-area">
+                <?php dynamic_sidebar( 'five-footer-widget-area' ); ?>
+            </li><!-- .fourth .widget-area -->
+        </ul><!-- #fatfooter -->
+        <?php 
+        elseif (   is_active_sidebar( 'first-footer-widget-area'  )
+            && is_active_sidebar( 'second-footer-widget-area' )
+            && is_active_sidebar( 'third-footer-widget-area'  )
+            && is_active_sidebar( 'fourth-footer-widget-area' )
+            && ! is_active_sidebar( 'five-footer-widget-area' )
+            && ! is_active_sidebar( 'six-footer-widget-area' )
+        ) : ?>
+         
+        <ul class="" role="complementary">
+            <li class="col-2 first quarter left widget-area">
+                <?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
+            </li><!-- .first .widget-area -->
+         
+            <li class="col-2 second quarter widget-area">
+                <?php dynamic_sidebar( 'second-footer-widget-area' ); ?>
+            </li><!-- .second .widget-area -->
+         
+            <li class="col-2 third quarter widget-area">
+                <?php dynamic_sidebar( 'third-footer-widget-area' ); ?>
+            </li><!-- .third .widget-area -->
+         
+            <li class="col-2 fourth quarter right widget-area">
+                <?php dynamic_sidebar( 'fourth-footer-widget-area' ); ?>
+            </li><!-- .fourth .widget-area -->
+        </ul><!-- #fatfooter -->
+        <?php 
+        elseif ( is_active_sidebar( 'first-footer-widget-area'  )
+            && is_active_sidebar( 'second-footer-widget-area' )
+            && is_active_sidebar( 'third-footer-widget-area'  )
+            && ! is_active_sidebar( 'fourth-footer-widget-area' )
+            && ! is_active_sidebar( 'five-footer-widget-area' )
+            && ! is_active_sidebar( 'six-footer-widget-area' )
+        ) : ?>
+        <ul class="" role="complementary">
+            <li class="col-2 first one-third left widget-area">
+                <?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
+            </li><!-- .first .widget-area -->
+         
+            <li class="col-2 second one-third widget-area">
+                <?php dynamic_sidebar( 'second-footer-widget-area' ); ?>
+            </li><!-- .second .widget-area -->
+         
+            <li class="col-2 third one-third right widget-area">
+                <?php dynamic_sidebar( 'third-footer-widget-area' ); ?>
+            </li><!-- .third .widget-area -->
+         
+        </ul><!-- #fatfooter -->
+        <?php
+        elseif ( is_active_sidebar( 'first-footer-widget-area'  )
+            && is_active_sidebar( 'second-footer-widget-area' )
+            && ! is_active_sidebar( 'third-footer-widget-area'  )
+            && ! is_active_sidebar( 'fourth-footer-widget-area' )
+            && ! is_active_sidebar( 'five-footer-widget-area' )
+            && ! is_active_sidebar( 'six-footer-widget-area' )
+        ) : ?>
+        <ul class="" role="complementary">
+            <li class="col-2 first half left widget-area">
+                <?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
+            </li><!-- .first .widget-area -->
+         
+            <li class="col-2 second half right widget-area">
+                <?php dynamic_sidebar( 'second-footer-widget-area' ); ?>
+            </li><!-- .second .widget-area -->
+         
+        </ul><!-- #fatfooter -->
+        <?php
+        elseif ( is_active_sidebar( 'first-footer-widget-area'  )
+            && ! is_active_sidebar( 'second-footer-widget-area' )
+            && ! is_active_sidebar( 'third-footer-widget-area'  )
+            && ! is_active_sidebar( 'fourth-footer-widget-area' )
+            && ! is_active_sidebar( 'five-footer-widget-area' )
+            && ! is_active_sidebar( 'six-footer-widget-area' )
+        ) :
+        ?>
+        <ul class="" role="complementary">
+            <li class="col-2 first full-width widget-area">
+                <?php dynamic_sidebar( 'first-footer-widget-area' ); ?>
+            </li><!-- .first .widget-area -->
+         
+        </ul><!-- #fatfooter -->
+    <?php //end of all sidebar checks.
+    endif;endif;?>      
     </div>
 </div>
 <div class="footer-end">
--- wp-content/themes/luxroyal/functions.php
+++ wp-content/themes/luxroyal/functions.php
@@ -410,7 +410,7 @@
 			'wpb_widget', 
 
 			// Widget name will appear in UI
-			__('WPBeginner Widget Blog', 'wpb_widget_domain'), 
+			__('Widget Blog', 'wpb_widget_domain'), 
 
 			// Widget description
 			array( 'description' => __( 'Sample widget based on WPBeginner Tutorial', 'wpb_widget_domain' ), ) 
@@ -429,6 +429,7 @@
 		// This is where you run the code and display the output
 		$args = array(
 				"post_type" 		=> 'blog',
+				'post_status'		=> array( 'publish' ),
 				"posts_per_page" 	=> '5',
 				"orderby"			=> 'date',
 				"order"				=> 'desc',
@@ -479,4 +480,73 @@
 		register_widget( 'wpb_widget' );
 	}
 	add_action( 'widgets_init', 'wpb_load_widget' );
+
+
+	/********************************************************* Creating the widget product*******************************************************/
+	class MyNewWidget extends WP_Widget {
+
+		function MyNewWidget() {
+			// Instantiate the parent object
+			parent::__construct( false, 'Widget Products' );
+		}
+
+		public function widget( $args, $instance ) {
+			$title = apply_filters( 'widget_title', $instance['title'] );
+			// before and after widget arguments are defined by themes
+			echo $args['before_widget'];
+			if ( ! empty( $title ) )
+			echo $args['before_title'] . $title . $args['after_title'];
+
+			// This is where you run the code and display the output
+			$args = array(
+					"post_type" 		=> 'product',
+					'post_status'		=> array( 'publish' ),
+					"posts_per_page" 	=> '5',
+					"orderby"			=> 'date',
+					"order"				=> 'desc',
+					);
+				// var_dump($args);
+			// The Query
+			$the_query = new WP_Query( $args );
+
+	    		if($the_query->have_posts()){
+					$i = 1;
+					echo '<div><ul>';
+					while($the_query->have_posts()){
+						$the_query->the_post();
+						echo '<li><a href="'.get_the_permalink().'">'.get_the_title().'</a></li>';
+					}
+					echo '</ul></div>';
+				}	
+			echo $args['after_widget'];
+		}
+
+		function update( $new_instance, $old_instance ) {
+			$instance = array();
+			$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+			return $instance;
+		}
+
+		function form( $instance ) {
+			if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+			}
+			else {
+			$title = __( 'New title', 'wpb_widget_domain' );
+			}
+			// Widget admin form
+			?>
+			<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+			</p>
+			<?php 
+		}
+	}
+
+	function myplugin_register_widgets() {
+		register_widget( 'MyNewWidget' );
+	}
+
+	add_action( 'widgets_init', 'myplugin_register_widgets' );
 ?>
